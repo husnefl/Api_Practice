4e96a02585a1a158c8635ac23612a9171cd3d4aa
husnefl <husneakincioglu@gmail.com>
2022-11-17 06:43:00 -0500

    We are learning Api

---
+++ src/test/java/test/Api_GetRequest.java
@@ -0,0 +1,37 @@
+package test;
+
+import netscape.javascript.JSObject;
+import org.json.JSONObject;
+import org.junit.Test;
+
+public class Api_GetRequest {
+ /*
+        Given
+            https://restful-booker.herokuapp.com/booking/101
+        When
+            User sends a GET Request to the url
+        Then
+            HTTP Status Code should be 200
+        And
+            Content Type should be application/json
+        And
+            Status Line should be HTTP/1.1 200 OK
+     */
+   @Test
+    public void get01(){
+
+   }
+
+    //1-Request URL ve Body'i olustur
+
+    String url= "https://restful-booker.herokuapp.com/booking/101";
+
+  // 2-Expected data olustur
+ /*
+ Json object uzerinden bir object olusturacagiz
+*/
+
+
+
+
+}
